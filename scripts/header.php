5abd0169de7c680147352e27317c9770b99a5a26
HilmanNurfauzan <nurfauzan457@gmail.com>
2025-05-20 13:43:21 +0700

    Login, Regist, dan Index

---
+++ scripts/header.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+echo '
+<link rel="stylesheet" href="/Jago_library%20Program/css/styles.css">
+<div class="header-container">
+    <div class="logo">
+        <a href="index.php">Jago Library</a>
+    </div>
+    <div class="footer-links">
+        <ul>
+            <li><a href="about.php">About</a></li>
+            <li><a href="book.php">Book</a></li>
+            <li><a href="authors.php">Authors</a></li>
+            <li><a href="geners.php">Geners</a></li>
+            <li><a href="lists.php">Lists</a></li>
+        </ul>
+    </div>
+    <div class="user-options">';
+    
+if (isset($_SESSION['username'])) {
+    echo '
+    <div class="profile-icon">
+        <a href="profile.php"><img src="/Jago_library%20Program/images/footers/profile.png" alt="Profile"></a>
+    </div>';
+} else {
+    echo '
+   <div class="auth-buttons">
+            <a href="login.php" class="login-btn">Login</a>
+            <a href="registration.php" class="signup-btn">Sign Up</a>';
+}
+
+echo '
+    </div>
+</div>
+';
+?>
---
+++ scripts/pinjam.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pinjam | Jago_library</title>
+    <link rel="stylesheet" href="/Silib/Jago_library%20Program/css/styles.css">
+</head>
+<body>
+    <header>
+        <?php include "header.php"; ?>
+    </header>
+    
+   <div class="book-image-container">
+                <img src="/Silib/Jago_library%20Program/images/harry-potter2.jpg" alt="Harry Potter and The Sorcerer's Stone" />
+            <div class="book-info">
+                            <h1 class="book-title">Harry Potter And The Sorcerer's Stone</h1>
+                            <p class="book-author">By Joan Rowling</p>
+                            
+                            <div class="book-rating">
+                                <span class="rating-stars">⭐⭐⭐⭐⭐</span>
+                                <span class="rating-count">5</span>
+                                </div>
+                            </div>
+
+                    <div class="book-details">
+                        <span class="detail-label">Original</span>
+                        <span class="detail-value">Harry Potter and the Sorcerer's Stone</span>
+
+                        <span class="detail-label">Format</span>
+                        <span class="detail-value">170x215 mm</span>
+
+                        <span class="detail-label">Number of pages</span>
+                        <span class="detail-value">348</span>
+
+                        <span class="detail-label">Year of issue</span>
+                        <span class="detail-value">2003</span>
+
+                        <span class="detail-label">ISBN</span>
+                        <span class="detail-value">5-353-01339-5</span>
+
+                        <span class="detail-label">Circulation</span>
+                        <span class="detail-value">11000</span>
+                    </div>
+                    
+                <div class="purchase-section">
+                    <p class="availability">Dispatched from the UK in 3 business days</p>
+                    <div class="purchase-controls">
+                        <button class="add-to-cart-btn">Pinjam</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <footer>
+        <?php include "footer.php"; ?>
+    </footer>
+</body>
+</html>
