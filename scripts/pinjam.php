3bd9c14aacede1039265522cd6b9018ee5210a3a
novilarya <arya.novila@gmail.com>
2025-05-27 12:25:44 +0700

    Fix Pinjam

--- scripts/pinjam.php
+++ scripts/pinjam.php
@@ -30,42 +30,7 @@
 <body>
     <header>
         <?php include "header.php"; ?>
-    </header>
-    <div class="book-image-container">
-        <img src="/images/harry-potter5.jpg" alt="Harry Potter and The Sorcerer's Stone" />
-        <span class="type-badge">Tersedia</span>
-        <div class="book-info-wrapper">
-            <div class="book-info">
-                <h1 class="book-title">Harry Potter And The Sorcerer's Stone</h1>
-                <p class="book-author">By JK Rowling</p>
-                <div class="book-rating">
-                    <span class="rating-stars">⭐⭐⭐⭐⭐</span>
-                </div>
-            </div>
-
-            <div class="book-details">
-                <div class="detail-row">
-                    <span class="detail-label">Ukuran</span>
-                    <span class="detail-value">170 x 215 mm</span>
-                </div>
-                <div class="detail-row">
-                    <span class="detail-label">Jumlah Halaman</span>
-                    <span class="detail-value">348</span>
-                </div>
-                <div class="detail-row">
-                    <span class="detail-label">Tahun Terbit</span>
-                    <span class="detail-value">2003</span>
-                </div>
-                <div class="detail-row">
-                    <span class="detail-label">ISBN</span>
-                    <span class="detail-value">5-353-01339-5</span>
-                </div>
-                <div class="detail-row">
-                    <span class="detail-label">Cetakan</span>
-                    <span class="detail-value">11000</span>
-                </div>
-            </div>
-            
+    </header>    
     <div class="book-image-container">
         <?php while($row = $buku->fetch_assoc()) { ?>
             <img src="/images/<?php echo $row['cover_buku']; ?>" alt="Harry Potter and The Sorcerer's Stone" />
